270356009ee58af6c3b2094bf3302bc6f4430615
Moyo75 <moyojohn188@gmail.com>
2022-11-25 11:06:07 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const API = "https://craterapi.com/api/package/search";
+
+  const resOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ msisdn: "" }),
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(API, resOptions);
+      const data = await res.json();
+
+      setProducts(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ol>
+        {products.map((product) => (
+          <li key={product.id}>{product.ticker}</li>
+        ))}
+      </ol>
     </div>
   );
 }
